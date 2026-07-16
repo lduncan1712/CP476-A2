016191000a5bedcc9b5f8a34aa8853e40e9c3fb8
Lucas <lduncan1712@gmail.com>
2026-07-16 12:24:34 -0400

    Initial Commit: Making File Shells + First Half Of Backend

--- backend/students.php
+++ backend/students.php
@@ -1,11 +1,15 @@
 <?php
+
+//Header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 try {
 
+    //Connect To Database
     $pdo = new PDO("mysql:host=mysql;dbname=college", "___", "____");
 
+    //Query Students
     $stmt = $pdo->query("SELECT * FROM students");
 
     $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
